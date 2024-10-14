0ce1887e068bb747cbf3506d1d69d5a2748f6aa0
mictori <mmictori@gmail.com>
2024-10-14 16:46:54 +0200

    Change readme

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# React + Vite
+## Task
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+-   Search field
+-   If there are too many (over 10) countries that match the query, then the user is prompted to make their query more specific
+-   If there are ten or fewer countries, but more than one, then all countries matching the query are shown
+-   When there is only one country matching the query, then the basic data of the country (eg. capital and area), its flag and the languages spoken are shown
+-   Button next to the name of the country, which when pressed shows the view for that country
+-   Add to the view showing the data of a single country
 
-Currently, two official plugins are available:
+### Not implemented yet
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+-   Weather info
--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,38 @@ import CountriesList from './Components/CountriesList';
 
 const App = () => {
 	const baseURL = 'https://studies.cs.helsinki.fi/restcountries/api/all';
-	const [searchValue, setSearchValue] = useState(null);
+	const [searchValue, setSearchValue] = useState('');
   const [countriesData, setCountriesData] = useState([])
   let matchedCountries = []
 
   const handleChangeInput = (event) => {
     event.target.value
     ? setSearchValue(event.target.value)
-    : setSearchValue(null)
+    : setSearchValue('')
   }
-
+  console.log('value', searchValue);
+  
   useEffect(() => {
     axios
       .get(baseURL)
       .then((response) => {
         setCountriesData(response.data)
       })
-    }, [searchValue])
+    }, [])
+    console.log('countries', countriesData);
+    
 
-    if (searchValue !== null) {
+    if (searchValue) {
       matchedCountries = countriesData.filter((c) => c.name.common.toLowerCase().includes(searchValue.toLowerCase()))
       console.log('matched', matchedCountries);
     }
 
 	return (
 		<div>
-			<h1>Countries and weather</h1>
+			<h1>Countries search</h1>
 			<div>
 				<label htmlFor='search'><span className='bold'>find countries</span></label>
-				<input id='search' onChange={handleChangeInput} placeholder='search...'></input>
+				<input id='search' value={searchValue} onChange={handleChangeInput} placeholder='search...'></input>
 			</div>
       <CountriesList countries={matchedCountries} countryToShow={setSearchValue}/>
 		</div>
